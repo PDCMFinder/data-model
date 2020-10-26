99d0575e13285f2ab2423d8313dba2411c7b6cd4
afollette <follette.alex@gmail.com>
2020-10-26 15:18:22 +0000

    Deleted rest of project. Updated POM to work independently.

--- data-services/src/main/java/org/pdxfinder/services/email/EmailConstant.java
+++
@@ -1,9 +0,0 @@
-package org.pdxfinder.services.email;
-
-/*
- * Created by abayomi on 16/08/2019.
- */
-public class EmailConstant {
-    public static final String SMTP_AUTH = "mail.smtp.auth";
-    public static final String SMTP_STARTLS = "mail.smtp.starttls.enable";
-}
