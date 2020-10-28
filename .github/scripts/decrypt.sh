99c8887ca80c716019210e1a6b6ba73f384379f2
afollette <follette.alex@gmail.com>
2020-10-28 13:41:59 +0000

    changed uri

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -2,4 +2,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg2 --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output=./pipeline.bash ./pipeline.sh.gpg
+--output=./pipeline.bash ./.github/scripts/pipeline.sh.gpg
