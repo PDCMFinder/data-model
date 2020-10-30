917c16b2c542114fc34fb164ec84e8e105dfcb31
afollette <follette.alex@gmail.com>
2020-10-30 11:40:47 +0000

    started new deployment strategy with Gitlab.
    Because:
    - these are the institution standards
    
    Removed .github directory. Wrote a .gitlab-ci.yml file for gitlab

--- .github/scripts/decrypt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg2 --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output=./pipeline.bash ./.github/scripts/pipeline.sh.gpg
